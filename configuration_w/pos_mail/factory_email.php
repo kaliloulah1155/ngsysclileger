cf8c576b2b85239ebef0c3616d13eaa4b40dad1e
kaliloulah <39620950+kaliloulah1155@users.noreply.github.com>
2022-02-22 18:20:06 +0000

    maj du mailing

---
+++ configuration_w/pos_mail/factory_email.php
@@ -0,0 +1,24 @@
+<?php
+
+ //appel de la class permettant de parametrer la requete POST CURL
+//NB : Veuillez configurer CURL au prealable dans apache avant son utilisation (voir documentat apache)  
+//Veuillez configurer egalement le htaccess dans apache
+ include('curllib.php');
+
+class FactoryMail{
+	function factmail(string $emailput,string $contentTitre,string $output):?string{
+		$resp=new LibCurl; 
+		$data_array =array(
+		    'email'=>explode(" ",$emailput),
+		    'subject'=>$contentTitre,
+		    'body'=>$output
+		  );
+		$response= $resp->postlibCurl('transactional',$data_array);
+		$data_response= json_encode($response);
+        return  $data_response;
+     }
+}
+
+
+
+ 
